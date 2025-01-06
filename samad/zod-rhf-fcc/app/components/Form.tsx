d58fece64d432df62f1aa035ea898ec80bb44b79
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-01-07 00:12:49 +0500

    1212am 07 01 2025

--- samad/zod-rhf-fcc/app/components/Form.tsx
+++ samad/zod-rhf-fcc/app/components/Form.tsx
@@ -3,6 +3,7 @@ import { FormData, UserSchema, ValidFieldNames } from "@/types";
 // import FormField from "./FormField";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import FormField from "./FormField";
 
 function Form() {
   const {
@@ -14,16 +15,49 @@ function Form() {
     resolver: zodResolver(UserSchema), // Apply the zodResolver
   });
 
+  // const onSubmit = async (data: FormData) => {
+  //   try {
+  //     const response = await axios.post("/api/form", {
+  //       email: "Not an email",
+  //       githubUrl: "Not a URL",
+  //       yearsOfExperience: "Hello",
+  //       password: 1234,
+  //       confirmPassword: 1234,
+  //     });
+  //     const response = await axios.post("/api/form", data); // Make a POST request
+  //     const { errors = {} } = response.data; // Destructure the 'errors' property from the response data
+
+  //     // Define a mapping between server-side field names and their corresponding client-side names
+  //     const fieldErrorMapping: Record<string, ValidFieldNames> = {
+  //       email: "email",
+  //       githubUrl: "githubUrl",
+  //       yearsOfExperience: "yearsOfExperience",
+  //       password: "password",
+  //       confirmPassword: "confirmPassword",
+  //     };
+
+  //     // Find the first field with an error in the response data
+  //     const fieldWithError = Object.keys(fieldErrorMapping).find(
+  //       (field) => errors[field]
+  //     );
+
+  //     // If a field with an error is found, update the form error state using setError
+  //     if (fieldWithError) {
+  //       // Use the ValidFieldNames type to ensure the correct field names
+  //       setError(fieldErrorMapping[fieldWithError], {
+  //         type: "server",
+  //         message: errors[fieldWithError],
+  //       });
+  //     }
+  //   } catch (error) {
+  //     alert("Submitting form failed!");
+  //   }
+  // };
+  
+
   const onSubmit = async (data: FormData) => {
     try {
-      const response = await axios.post("/api/form", {
-        email: "Not an email",
-        githubUrl: "Not a URL",
-        yearsOfExperience: "Hello",
-        password: 1234,
-        confirmPassword: 1234,
-      });
-      // const response = await axios.post("/api/form", data); // Make a POST request
+      const response = await axios.post("/api/form", data); // Make a POST request
       const { errors = {} } = response.data; // Destructure the 'errors' property from the response data
 
       // Define a mapping between server-side field names and their corresponding client-side names
@@ -52,7 +86,6 @@ function Form() {
       alert("Submitting form failed!");
     }
   };
-  
 
   return (
       <form onSubmit={handleSubmit(onSubmit)}>
